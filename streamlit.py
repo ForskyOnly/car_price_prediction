f6900e2a3d38f37c62e6e99df514aad925a9f15a
ForskyOnly <foulseumsky@gmail.com>
2023-04-25 17:36:08 +0200

    1stepbefore streamlit

---
+++ streamlit.py
@@ -0,0 +1,11 @@
+import pickle
+import streamlit as st
+
+
+html_temp = """
+    <div style="background:#025246 ;padding:10px">
+    <h2 style="color:white;text-align:center;"> Estimez le prix d'une voitre
+    </h2></div>
+    
+    """
+st.markdown(html_temp, unsafe_allow_html=True)
