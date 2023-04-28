37e3e2802966650d611739e00faf0dac840c2ef5
ForskyOnly <foulseumsky@gmail.com>
2023-04-28 10:00:11 +0200

    stramlit deploy

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
-from sklearn.ensemble import RandomForestRegressor
+import sklearn
 
 
 st.set_page_config(page_title="Car Price Estimator", page_icon="🚘", layout="wide")
